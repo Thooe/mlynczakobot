1c01a5d7bfba10e1c8580780d3e8f91d6f55a3c4
Thooe <fudeiq@o2.pl>
2017-07-02 00:00:41 +0200

    new commands

--- mlynczakobot.py
+++ mlynczakobot.py
@@ -1,7 +1,9 @@
 from discord.ext import commands
 from datetime import datetime
 from imdbpie import Imdb
+from coinmarketcap import Market
 
+import time
 import discord
 import random
 import ctypes
@@ -17,6 +19,7 @@ root = tree.getroot()
 def cc(): return os.system('cls')
 
 
+timer_start = time.time()
 bot = commands.Bot(command_prefix='!')
 
 list_of_fastfoods = ['kfc', 'mcdonald', 'kebab', 'fastfood', 'burgerking', 'wendys']
@@ -48,13 +51,18 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
+    m = message.content.lower()
     """react to fastfood restaurants"""
-    if message.content in list_of_fastfoods:
+    if m in list_of_fastfoods:
         await bot.add_reaction(message, '🤖')
     """react to fuck you message"""
-    if message.content == 'fuck you':
+    if m == 'fuck you':
         await bot.add_reaction(message, '🔫')
         await bot.send_message(message.channel, 'fuck you too')
+    """fuck supreme"""
+    if 'supreme' in m:
+        if not message.author.bot:
+            await bot.send_message(message.channel, 'fuck supreme❗')
     await bot.process_commands(message)
 
 """when someone join server do stuff"""
@@ -170,14 +178,14 @@ async def server(ctx):
 
 
 @bot.command(pass_context=True)
-async def clear(amount: int = 100):
-    await bot.say(ctx.message.channel, '')
+async def clear(ctx, amount: int = 10):
+    await bot.purge_from(ctx.message.channel, limit=amount)
 
 """stock"""
 
 
 @bot.command(pass_context=True)
-async def stock(currency: str = None):
+async def stock(currency: str=None):
     await bot.say(ctx.message.channel, '')
 
 
@@ -187,7 +195,7 @@ idb = Imdb()
 
 
 @bot.command(pass_context=True)
-async def imdb(ctx, *, image: str = None):
+async def imdb(ctx, *, image: str=None):
     if not image:
         await bot.send_message(ctx.message.channel,
                                '{} ⚠ You need to specify name of movie or tv show! ⚠\n\n Example: !imdb La La Land'
@@ -289,7 +297,7 @@ async def rud(ctx):
 
 
 @bot.command(pass_context=True)
-async def ud(ctx, *, words: str = None):
+async def ud(ctx, *, words: str=None):
     await udi(ctx, words)
 
 """urban dictionary"""
@@ -317,6 +325,79 @@ async def udi(ctx, words=None, random=False):
         if words:
             await bot.send_message(ctx.message.channel, '{} ⚠ Nie znaleziono słowa ⚠'.format(ctx.message.author.mention))
 
+
+def getdlist(data, arg, index=0):
+    return data[index].get(arg)
+
+
+"""Cryptocurrency Info"""
+
+coinmarketcap = Market()
+
+
+@bot.command(pass_context=True)
+async def crypto(ctx, currency, convert='USD'):
+    data = []
+    try:
+        data = coinmarketcap.ticker(currency, convert=convert)
+    except:
+        await bot.send_message(ctx.message.channel,
+                               '⚠{} Currency {} was not found!⚠'.format(ctx.message.author.mention, currency))
+        return
+
+    em = discord.Embed(colour=0xFFD219, title=getdlist(data, 'name'),
+                       url='https://coinmarketcap.com/currencies/{}/'.format(getdlist(data, 'id')))
+
+    last_updated = time.ctime(int(getdlist(data, 'last_updated')))
+    price = getdlist(data, 'price_usd')
+
+    if convert != 'USD':
+        price = getdlist(data, 'price_{}'.format(convert.lower()))
+
+    if not price:
+        await bot.send_message(ctx.message.channel,
+                               '⚠{} Currency convert type {} was not found!⚠'.format(ctx.message.author.mention, convert))
+        return
+
+    (em
+     .add_field(name='ID:', value=getdlist(data, 'id'), inline=True)
+     .add_field(name='Symbol:', value=getdlist(data, 'symbol'), inline=True)
+     .add_field(name='Rank:', value=getdlist(data, 'rank'), inline=True)
+     .add_field(name='Price:', value='{} {}'.format(price, convert.upper()), inline=True)
+     .add_field(name='BTC Price:', value=getdlist(data, 'price_btc') + ' BTC', inline=True)
+     .add_field(name='Change 1h:', value=getdlist(data, 'percent_change_1h') + ' \%', inline=True)
+     .add_field(name='Change 24h:', value=getdlist(data, 'percent_change_24h') + ' \%', inline=True)
+     .add_field(name='Change 7d:', value=getdlist(data, 'percent_change_7d') + ' \%', inline=True)
+     .add_field(name='Last updated:', value=last_updated, inline=True)
+     .set_footer(text=ctx.message.author, icon_url=ctx.message.author.avatar_url))
+
+    await bot.send_message(ctx.message.channel, embed=em)
+
+"""Cryptocurrency Top list info"""
+
+
+@bot.command(pass_context=True)
+async def cryptotop(ctx, convert='USD'):
+    data = coinmarketcap.ticker(limit=10, convert='EUR')
+
+    top10 = ''
+    for i, e in enumerate(data):
+        top10 = top10 + '{}. {}\n\n'.format(i + 1, e.get('name'))
+
+    em = discord.Embed(colour=0xFFD219, title='Top 10 Crypto currencies.',
+                       url='https://coinmarketcap.com/', description=top10)
+
+    await bot.send_message(ctx.message.channel, embed=em)
+
+"""Bot uptime"""
+
+
+@bot.command(pass_context=True)
+async def uptime(ctx):
+    uptime = time.time() - timer_start
+    t = int(uptime)
+    await bot.say('{:02}h {:02}m {:02}s'.format(t // 3600, t % 3600 // 60, t % 60))
+
 """russian roulette"""
 
 
@@ -335,5 +416,12 @@ async def rrol(ctx):
 @bot.command()
 async def roll(number: int=100):
     await bot.say('🎲 Wylosowano: {}'.format(random.randint(0, number)))
+
+"""echo"""
+
+
+@bot.command()
+async def echo(*, word):
+    await bot.say(word)
 
 bot.run(getToken('Discord'))
